48ec3acf931640f594eb1ca2c3f5e15897dc907f
nazam-007 <145904157+nazam-007@users.noreply.github.com>
2023-11-02 17:58:56 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,101 @@
+var curs=document.querySelector("#cursor");
+var cblr=document.querySelector("#cursor-blur");
+document.addEventListener("mousemove",function(dets){
+    curs.style.left=dets.x+"px"
+    curs.style.top=dets.y+"px"
+    cblr.style.left=dets.x-250 +"px"
+    cblr.style.top=dets.y-250 +"px"
+})
+
+
+
+
+
+
+
+
+gsap.to("#nav",{
+    backgroundColor:"#000",
+    duration:0.5,
+    height:"110px",
+    scrollTrigger:{
+        trigger:"#nav",
+        scroller:"body",
+        //markers:true,
+        start:"top-10%",
+        end:"top -11%",
+        scrub:1
+    }
+})
+
+gsap.to("#main",{
+    backgroundColor:"#000",
+    scrollTrigger:{
+        trigger:"#main",
+        start:"top -25%",
+        end:"top -70%",
+        scrub:2
+    }
+})
+gsap.from("#about-us img,#about-us-in", {
+    y: 90,
+    opacity: 0,
+    duration: 1,
+    scrollTrigger: {
+      trigger: "#about-us",
+      scroller: "body",
+      // markers:true,
+      start: "top 70%",
+      end: "top 65%",
+      scrub: 1,
+    },
+  });
+  gsap.from(".card", {
+    scale: 0.8,
+    // opacity:0,
+    duration: 1,
+    stagger: 0.1,
+    scrollTrigger: {
+      trigger: ".card",
+      scroller: "body",
+      // markers:false,
+      start: "top 70%",
+      end: "top 65%",
+      scrub: 1,
+    },
+  });
+  gsap.from("#colon1", {
+    y: -70,
+    x: -70,
+    scrollTrigger: {
+      trigger: "#colon1",
+      scroller: "body",
+      // markers:true,
+      start: "top 55%",
+      end: "top 45%",
+      scrub: 4,
+    },
+  });
+  gsap.from("#colon2", {
+    y: 70,
+    x: 70,
+    scrollTrigger: {
+      trigger: "#colon1",
+      scroller: "body",
+      // markers:true,
+      start: "top 55%",
+      end: "top 45%",
+      scrub: 4,
+    },
+  });
+  gsap.from("#page4 h1", {
+    y: 50,
+    scrollTrigger: {
+      trigger: "#page4 h1",
+      scroller: "body",
+      // markers:true,
+      start: "top 75%",
+      end: "top 70%",
+      scrub: 3,
+    },
+  });
